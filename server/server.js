233aa58c6b7bd1a373b71e4899d4902ed6f900c9
JQ Bai <jianqingb@gmail.com>
2025-02-13 14:12:34 +0800

    test

--- server/server.js
+++ server/server.js
@@ -1,17 +1,11 @@
-const https = require("https");
+const http = require("http");
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
 
 const app = express();
 const host = "0.0.0.0"; // Bind to all available network interfaces
-const PORT = process.env.PORT || 3000; // Use the port provided by Render
-
-// Load SSL certificate and key from files
-const options = {
-    key: fs.readFileSync(path.join(__dirname, "../ssl/private.key")),
-    cert: fs.readFileSync(path.join(__dirname, "../ssl/certificate.crt"))
-};
+const port = process.env.PORT || 8000; // Use the port provided by Render
 
 // Serve static files from the "local" directory
 app.use(express.static(path.join(__dirname, "../local")));
@@ -21,14 +15,14 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../local/main.html"));
 });
 
-// Create HTTPS server
-const server = https.createServer(options, app);
+// Create HTTP server
+const server = http.createServer(app);
 
 // Start server
-server.listen(PORT, host, (err) => {
+server.listen(port, host, (err) => {
     if (err) {
         console.error("Error starting server:", err);
         process.exit(1);
     }
-    console.log(`Server is running on https://${host}:${PORT}`);
+    console.log(`Server is running on http://${host}:${port}`);
 });
