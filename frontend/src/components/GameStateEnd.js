caf8a400a17093944a6741c1f609022b1962aad2
JQ Bai <jianqingb@gmail.com>
2025-02-26 20:18:50 +0800

    GameStateEnd cleaned up

--- frontend/src/components/GameStateEnd.js
+++ frontend/src/components/GameStateEnd.js
@@ -1,12 +1,27 @@
-import React from 'react';
+/*
+    This is the end game state
+*/
+
+import React from 'react'; // Imports the React library
+import { DotLottieReact } from '@lottiefiles/dotlottie-react'; // Imports the DotLottieReact component from the DotLottieReact library
 
 const GameStateEnd = ({ winner, onReturnToTitle }) => {
-
+    // Renders JSX visual elements
     return (
         <div className="hero center">
+            <DotLottieReact
+                src="https://lottie.host/be15b006-9c70-427a-b438-2ff303d51a7f/Osti3Ta6ZB.lottie"
+                loop
+                autoplay
+                style={{ width: 'var(--size-xlarge2)', height: 'var(--size-xlarge2)' }}
+            />
+            <br />
+            <br />
             <h2>Game Over</h2>
             <h3>The winner is {winner}</h3> {/* Display the winner */}
             <br />
+
+            {/* Calls the onReturnToTile function */}
             <button className="cta" onClick={onReturnToTitle}>Return to Title Screen</button> {/* Return to Title Screen button */}
         </div>
     );
